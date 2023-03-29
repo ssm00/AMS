1123c017d37069a669c4ae55a4c44a4fd91bc852
ssm00 <97657265+ssm00@users.noreply.github.com>
2023-03-29 14:28:57 +0900

    init commit

---
+++ AMS-backend/src/main/java/com/ams/amsbackend/AmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ams.amsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmsBackendApplication.class, args);
+    }
+
+}
