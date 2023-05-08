ec7a0cd296036f56d3876c65ce3bc1180034a9a8
Yoo Byeong-gue <ybg653924@gmail.com>
2023-05-08 12:40:52 +0900

    Test: Ex1

---
+++ AMS-backend/src/main/java/com/ams/amsbackend/controller/dto/Ex1.java
@@ -0,0 +1,10 @@
+package com.ams.amsbackend.controller.dto;
+
+public class Ex1 {
+    private int a = 0;
+    private int b = 0;
+
+    public int add(int num1, int num2){
+        return num1+num2+a+b;
+    }
+}
