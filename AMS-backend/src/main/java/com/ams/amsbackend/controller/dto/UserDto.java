151bd210f6bc1d03db4de28cbbb29bf4d87210e5
Yoo Byeong-gue <ybg653924@gmail.com>
2023-05-08 12:35:20 +0900

    Test: User

---
+++ AMS-backend/src/main/java/com/ams/amsbackend/controller/dto/UserDto.java
@@ -0,0 +1,36 @@
+package com.ams.amsbackend.controller.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+public class UserDto {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public class PostInputAnswersReq {
+        private Long userId;
+        private int examNumber;
+        private String examSubject;
+        private String studentAnswer;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public class PostGradeCardInfoReq {
+        private int examNumber;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @Builder
+    public class PostGradeCardInfoRes {
+        private int totalStudents;
+        private int studentRank;
+        private int perfectScore;
+        private int studentScore;
+        private int examNumber;
+        private String studentAnswer;
+    }
+}
