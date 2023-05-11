bc286d1c2b533374d21c050dae2ed1322f0fde5d
ssm00 <97657265+ssm00@users.noreply.github.com>
2023-05-11 15:51:49 +0900

    Update PostTopFiveStudentInfoRes.java

--- AMS-backend/src/main/java/com/ams/amsbackend/controller/dto/PostTopFiveStudentInfoRes.java
+++ AMS-backend/src/main/java/com/ams/amsbackend/controller/dto/PostTopFiveStudentInfoRes.java
@@ -1,4 +1,14 @@
 package com.ams.amsbackend.controller.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
 public class PostTopFiveStudentInfoRes {
+    private Integer examNumber;
+    //5명의 이름,점수,등수 리스트로반환
+    private List<EachStudentInfo> top5StudentList;
 }
