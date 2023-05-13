58f7846615d8d49d20a347778390cf28a5b2c01c
ssm00 <97657265+ssm00@users.noreply.github.com>
2023-05-14 02:04:45 +0900

    examSubject추가

--- AMS-backend/src/main/java/com/ams/amsbackend/controller/dto/PostTopFiveStudentInfoRes.java
+++ AMS-backend/src/main/java/com/ams/amsbackend/controller/dto/PostTopFiveStudentInfoRes.java
@@ -1,14 +1,17 @@
 package com.ams.amsbackend.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 @Builder
 public class PostTopFiveStudentInfoRes {
     private Integer examNumber;
+    private String examSubject;
     //5명의 이름,점수,등수 리스트로반환
     private List<EachStudentInfo> top5StudentList;
 }
