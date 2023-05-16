11ea11ee0e23088eb06dfae71e9bf1cca1cc3495
ssm00 <97657265+ssm00@users.noreply.github.com>
2023-05-17 03:27:52 +0900

    회원가입 +  주석확인

--- AMS-backend/src/main/java/com/ams/amsbackend/service/UserService.java
+++ AMS-backend/src/main/java/com/ams/amsbackend/service/UserService.java
@@ -1,12 +1,11 @@
 package com.ams.amsbackend.service;
 
+import com.ams.amsbackend.controller.dto.PostSignUpRes;
 import com.ams.amsbackend.controller.dto.UserDto;
-import com.ams.amsbackend.domain.ExamAnswerEntity;
-import com.ams.amsbackend.domain.StudentAnswerEntity;
-import com.ams.amsbackend.domain.StudentEntity;
-import com.ams.amsbackend.repository.ExamAnswerRepository;
-import com.ams.amsbackend.repository.StudentAnswerRepository;
-import com.ams.amsbackend.repository.StudentRepository;
+import com.ams.amsbackend.domain.*;
+import com.ams.amsbackend.repository.*;
+import com.ams.amsbackend.util.BaseException;
+import com.ams.amsbackend.util.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,34 +19,67 @@ public class UserService {
     private final StudentAnswerRepository studentAnswerRepository;
     private final ExamAnswerRepository examAnswerRepository;
     private final StudentRepository studentRepository;
+    private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
 
-    public UserDto.PostGradeCardInfoRes getGradeCardeInfo(Long userId, int examNumber, String examSubject) {
-        // output = 전체 응시 학생 수, 등수(studentRank), 만점(100), 점수(studentScore), 회차(examNumber),
-        // 각 번호별 정보(정답(ExamAnswer.examAnswer, 학생이 적은 답(StudentAnswer.studentAnswer), 오답률)
-        int totalStudent = this.studentAnswerRepository.countStudentAnswerEntitiesByExamNumberAndExamSubject(examNumber, examSubject);
-        ExamAnswerEntity examAnswerEntity = this.examAnswerRepository.findAllByExamNumberAndSubject(examNumber, examSubject);
-        String[] examAnswers = examAnswerEntity.getExamAnswer().split(",");
-        Optional<StudentEntity> studentEntity = this.studentRepository.findById(userId);
-        StudentAnswerEntity studentAnswerEntity = this.studentAnswerRepository.findByStudentEntityAndExamNumberAndExamSubject(studentEntity.get(), examNumber, examSubject);
-        String[] studentAnswers = studentAnswerEntity.getStudentAnswer().split(",");
-        float[] incorrectRate = new float[studentAnswers.length];
-        List<UserDto.EachProblemInfo> eachProblemInfoList = new ArrayList<>();
-        for(int i=0; i<examAnswers.length; i++){
-            eachProblemInfoList.add(new UserDto.EachProblemInfo(
-                    i+1,
-                    examAnswers[i],
-                    studentAnswers[i],
-                    incorrectRate[i]
-            ));
+//    public UserDto.PostGradeCardInfoRes getGradeCardeInfo(Long userId, int examNumber, Subject examSubject) {
+//        // output = 전체 응시 학생 수, 등수(studentRank), 만점(100), 점수(studentScore), 회차(examNumber),
+//        // 각 번호별 정보(정답(ExamAnswer.examAnswer, 학생이 적은 답(StudentAnswer.studentAnswer), 오답률)
+//        int totalStudent = this.studentAnswerRepository.countStudentAnswerEntitiesByExamNumberAndExamSubject(examNumber, examSubject);
+//        ExamAnswerEntity examAnswerEntity = this.examAnswerRepository.findAllByExamNumberAndSubject(examNumber, examSubject);
+//        String[] examAnswers = examAnswerEntity.getExamAnswer().split(",");
+//        Optional<StudentEntity> studentEntity = this.studentRepository.findById(userId);
+//        StudentAnswerEntity studentAnswerEntity = this.studentAnswerRepository.findByStudentEntityAndExamNumberAndExamSubject(studentEntity.get(), examNumber, examSubject);
+//        String[] studentAnswers = studentAnswerEntity.getStudentAnswer().split(",");
+//        float[] incorrectRate = new float[studentAnswers.length];
+//        List<UserDto.EachProblemInfo> eachProblemInfoList = new ArrayList<>();
+//        for(int i=0; i<examAnswers.length; i++){
+//            eachProblemInfoList.add(new UserDto.EachProblemInfo(
+//                    i+1,
+//                    examAnswers[i],
+//                    studentAnswers[i],
+//                    incorrectRate[i]
+//            ));
+//        }
+//        return new UserDto.PostGradeCardInfoRes(
+//                totalStudent,
+//                studentAnswerEntity.getStudentRank(),
+//                100,
+//                studentAnswerEntity.getStudentScore(),
+//                examNumber,
+//                examSubject,
+//                eachProblemInfoList
+//        );
+//    }
+
+
+    public StudentEntity signUpStudent(String userName, String email, String logInId, String password, String schoolName, SchoolType schoolType, Integer grade, String className) throws BaseException {
+        if (userName == null || email == null || logInId == null || password == null || schoolType == null || schoolName == null || grade == null || className == null) {
+            throw new BaseException(BaseResponseStatus.CREATE_STUDENT_DATA_NULL);
         }
-        return new UserDto.PostGradeCardInfoRes(
-                totalStudent,
-                studentAnswerEntity.getStudentRank(),
-                100,
-                studentAnswerEntity.getStudentScore(),
-                examNumber,
-                examSubject,
-                eachProblemInfoList
-        );
+        if (userRepository.existsByLoginId(logInId)) {
+            throw new BaseException(BaseResponseStatus.DUPLICATE_LOGINID);
+        }
+        StudentEntity studentEntity = new StudentEntity(logInId, userName, email, password, Role.ROLE_USER, schoolType, schoolName, grade, className);
+        return studentRepository.save(studentEntity);
     }
+    
+
+    /**
+     * 선생님 회원가입
+     * 가입시 userRepository에서 id확인
+     * save는 teacherRepository
+     */
+    public TeacherEntity signUpTeacher(String userName, String email, String logInId, String password, SchoolType schoolType, Integer manageGrade, Subject subject) throws BaseException {
+        if (userName == null || email == null || logInId == null || password == null || schoolType == null || manageGrade == null || subject == null) {
+            throw new BaseException(BaseResponseStatus.CREATE_TEACHER_DATA_NULL);
+        }
+        if (userRepository.existsByLoginId(logInId)) {
+            throw new BaseException(BaseResponseStatus.DUPLICATE_LOGINID);
+        }
+        TeacherEntity teacherEntity = new TeacherEntity(logInId, userName, email, password, Role.ROLE_USER, subject, manageGrade, schoolType);
+        return teacherRepository.save(teacherEntity);
+    }
+
+
 }
