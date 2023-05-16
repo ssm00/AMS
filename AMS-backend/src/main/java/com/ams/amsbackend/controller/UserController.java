7fb427b1a76253c8ac76fb9bd4b00dd38fd4b989
ssm00 <97657265+ssm00@users.noreply.github.com>
2023-05-17 03:23:13 +0900

    create signup + 주석확인

--- AMS-backend/src/main/java/com/ams/amsbackend/controller/UserController.java
+++ AMS-backend/src/main/java/com/ams/amsbackend/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.ams.amsbackend.controller;
 
+import com.ams.amsbackend.controller.dto.PostLogInReq;
+import com.ams.amsbackend.controller.dto.PostSignUpReq;
+import com.ams.amsbackend.controller.dto.PostSignUpRes;
 import com.ams.amsbackend.controller.dto.UserDto;
+import com.ams.amsbackend.domain.SchoolType;
+import com.ams.amsbackend.domain.Subject;
 import com.ams.amsbackend.service.UserService;
+import com.ams.amsbackend.util.BaseException;
 import com.ams.amsbackend.util.BaseResponse;
+import com.ams.amsbackend.util.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.*;
@@ -16,26 +23,53 @@ public class UserController {
     private final UserService userService;
 
 
-    @PostMapping("sing-up")
-    public BaseResponse<?> singUp() {
-        return null;
+    /**
+     * 회원가입
+     *  타입은 S(student),T(teacher)
+     */
+    @PostMapping("sign-up")
+    public BaseResponse<PostSignUpRes> singUp(@RequestBody PostSignUpReq signUpDto) {
+        if (signUpDto.getType().isBlank()) {
+            return new BaseResponse(BaseResponseStatus.NO_USER_TYPE);
+        }
+        String userName = signUpDto.getName();
+        String email = signUpDto.getEmail();
+        String logInId = signUpDto.getLogInId();
+        String password = signUpDto.getPassword();
+        SchoolType schoolType = signUpDto.getSchoolType();
+        try {
+            if (signUpDto.getType().equals("S")) {
+                String schoolName = signUpDto.getSchoolName();
+                String className = signUpDto.getClassName();
+                Integer grade = signUpDto.getGrade();
+                userService.signUpStudent(userName, email, logInId, password, schoolName, schoolType, grade, className);
+            } else if (signUpDto.getType().equals("T")) {
+                Integer manageGrade = signUpDto.getManageGrade();
+                Subject manageSubject = signUpDto.getManageSubject();
+                userService.signUpTeacher(userName, email, logInId, password, schoolType, manageGrade, manageSubject);
+            }
+        } catch (BaseException e) {
+            return new BaseResponse(e.getStatus());
+        }
+        PostSignUpRes response = new PostSignUpRes(userName, logInId, signUpDto.getType());
+        return new BaseResponse(response);
     }
 
     @PostMapping("log-in")
-    public BaseResponse<?> logIn() {
-        return null;
+    public BaseResponse<?> logIn(@RequestBody PostLogInReq logInDto) {
+
     }
 
-    @ResponseBody
-    @PostMapping("grade-card")
-    public BaseResponse<UserDto.PostGradeCardInfoRes> getGradeCardInfo(@RequestBody int examNumber, String examSubject) {
-        //개인 성적표(등수, 오답률, 점수)
-        // input = userId(Student), examNumber(시험 회차). examSubject(시험 과목)
-        // output = 전체 응시 학생 수, 등수(studentRank), 만점(100), 점수(studentScore), 회차(examNumber), 각 번호별 정보(정답(ExamAnswer.examAnswer, 학생이 적은 답(StudentAnswer.studentAnswer), 오답률)
-        Long userId = null;
-        UserDto.PostGradeCardInfoRes gradeCardInfo = this.userService.getGradeCardeInfo(userId, examNumber, examSubject);
-        return new BaseResponse<>(gradeCardInfo);
-    }
+//    @ResponseBody
+//    @PostMapping("grade-card")
+//    public BaseResponse<UserDto.PostGradeCardInfoRes> getGradeCardInfo(@RequestBody int examNumber, String examSubject) {
+//        //개인 성적표(등수, 오답률, 점수)
+//        // input = userId(Student), examNumber(시험 회차). examSubject(시험 과목)
+//        // output = 전체 응시 학생 수, 등수(studentRank), 만점(100), 점수(studentScore), 회차(examNumber), 각 번호별 정보(정답(ExamAnswer.examAnswer, 학생이 적은 답(StudentAnswer.studentAnswer), 오답률)
+//        Long userId = null;
+//        UserDto.PostGradeCardInfoRes gradeCardInfo = this.userService.getGradeCardeInfo(userId, examNumber, examSubject);
+//        return new BaseResponse<>(gradeCardInfo);
+//    }
 
     @GetMapping("grade-graph")
     public BaseResponse<UserDto.GetGradeGraphRes> getGradeGraphInfo() {
