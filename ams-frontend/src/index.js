215034e18dbec6f3b9ed298ddfe3ab4a40a45ed5
ssm00 <titan0724@mju.ac.kr>
2023-06-25 23:31:24 +0900

    0625
    create Router

--- ams-frontend/src/index.js
+++ ams-frontend/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/styles/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            {/* add routes with layouts */}
+            <Route path="/admin" component={Admin} />
+            <Route path="/auth" component={Auth} />
+            {/* add routes without layouts */}
+            <Route path="/landing" exact component={Landing} />
+            <Route path="/profile" exact component={Profile} />
+            <Route path="/" exact component={Index} />
+            {/* add redirect for first page */}
+            <Redirect from="*" to="/" />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
