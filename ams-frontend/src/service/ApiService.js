3b13a7e03a642cd286d09698896714fa3e702274
ssm00 <titan0724@mju.ac.kr>
2023-06-28 22:52:34 +0900

    Update ApiService.js

--- ams-frontend/src/service/ApiService.js
+++ ams-frontend/src/service/ApiService.js
@@ -2,23 +2,25 @@ import { API_BASE_URL } from "../app-config";
 const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+    // 헤더 설정
     let headers = new Headers({
         "Content-type": "application/json",
     });
-
+    //token 설정
     const accessToken = localStorage.getItem("ACCESS_TOKEN");
     if (accessToken && accessToken !== null) {
         headers.append("Authorization", "Bearer " + accessToken);
     }
-
     let options = {
         headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
+    //request가 있는경우 body 설정
     if (request) {
         options.body = JSON.stringify(request);
     }
+    //첫번째 then의 response는 받을데이터(json) 형태의 깡통 세팅, 두번째 then의 response는 받은데이터임
     return fetch(options.url, options)
         .then((response) =>
             response.json().then((json) => {
