415c35dfbe4b77fedc955f476cf3bb724784c877
ssm00 <titan0724@mju.ac.kr>
2023-07-06 18:02:15 +0900

    Update AppRouter.js

--- ams-frontend/src/AppRouter.js
+++ ams-frontend/src/AppRouter.js
@@ -6,12 +6,13 @@ import SignUp from "./auth/SignUp";
 class AppRouter extends React.Component {
     render() {
         return (
+            <>
             <div>
                 <Router>
                     <div>
                         {/* React-router-dom V6 이후 Switch -> routes로 바뀜  Route 안에 element 사용 */}
                         <Routes>
-                            <Route path="/" element={<TeacherMain />} />
+                            <Route path="/teacher" element={<TeacherMain />} />
                             <Route path="/users/*" element={<Login />} />
                             <Route path="/users/log-in" element={<Login />} />
                             <Route path="/users/sign-up" element={<SignUp />} />
@@ -19,6 +20,7 @@ class AppRouter extends React.Component {
                     </div>
                 </Router>
             </div>
+            </>
         );
     }
 }
