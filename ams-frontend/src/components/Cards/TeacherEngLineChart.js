7e252902167a49b57f9fd79db1dd20863e5cf7c5
ssm00 <titan0724@mju.ac.kr>
2023-07-06 18:02:45 +0900

    Teacher 성적 평균 그래프 완성

--- ams-frontend/src/components/Cards/TeacherEngLineChart.js
+++ ams-frontend/src/components/Cards/TeacherEngLineChart.js
@@ -4,113 +4,105 @@ import {call} from "../../service/ApiService";
 
 export  default function TeacherEngLineChart() {
   const [data, setData] = useState([]);
-  React.useEffect(() => {
-    call("/teachers/average-graph", "POST", {"grade" : 3, "examSubject" : "ENGLISH"}).then((response) => {
-      setData(response.data);
-    });
-    console.log(data)
-    var config = {
-      type: "line",
-      data: {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-        ],
-        datasets: [
-          {
-            label: new Date().getFullYear(),
-            backgroundColor: "#4c51bf",
-            borderColor: "#4c51bf",
-            data: [65, 78, 66, 44, 56, 67, 75],
-            fill: false,
-          },
-          {
-            label: new Date().getFullYear() - 1,
-            fill: false,
-            backgroundColor: "#fff",
-            borderColor: "#fff",
-            data: [40, 68, 86, 74, 56, 60, 87],
-          },
-        ],
+  var config = {
+    type: "line",
+    data: {
+      labels: [
+      ],
+      datasets: [
+        {
+          label: "평균1",
+          backgroundColor: "#4c51bf",
+          borderColor: "#4c51bf",
+          data: [],
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      title: {
+        display: false,
+        text: "Average Charts",
+        fontColor: "white",
       },
-      options: {
-        maintainAspectRatio: false,
-        responsive: true,
-        title: {
-          display: false,
-          text: "Sales Charts",
+      legend: {
+        labels: {
           fontColor: "white",
         },
-        legend: {
-          labels: {
-            fontColor: "white",
-          },
-          align: "end",
-          position: "bottom",
-        },
-        tooltips: {
-          mode: "index",
-          intersect: false,
-        },
-        hover: {
-          mode: "nearest",
-          intersect: true,
-        },
-        scales: {
-          xAxes: [
-            {
-              ticks: {
-                fontColor: "rgba(255,255,255,.7)",
-              },
-              display: true,
-              scaleLabel: {
-                display: false,
-                labelString: "Month",
-                fontColor: "white",
-              },
-              gridLines: {
-                display: false,
-                borderDash: [2],
-                borderDashOffset: [2],
-                color: "rgba(33, 37, 41, 0.3)",
-                zeroLineColor: "rgba(0, 0, 0, 0)",
-                zeroLineBorderDash: [2],
-                zeroLineBorderDashOffset: [2],
-              },
-            },
-          ],
-          yAxes: [
-            {
-              ticks: {
-                fontColor: "rgba(255,255,255,.7)",
-              },
-              display: true,
-              scaleLabel: {
-                display: false,
-                labelString: "Value",
-                fontColor: "white",
-              },
-              gridLines: {
-                borderDash: [3],
-                borderDashOffset: [3],
-                drawBorder: false,
-                color: "rgba(255, 255, 255, 0.15)",
-                zeroLineColor: "rgba(33, 37, 41, 0)",
-                zeroLineBorderDash: [2],
-                zeroLineBorderDashOffset: [2],
-              },
-            },
-          ],
-        },
+        align: "end",
+        position: "bottom",
       },
-    };
-    var ctx = document.getElementById("line-chart").getContext("2d");
-    window.myLine = new Chart(ctx, config);
+      tooltips: {
+        mode: "index",
+        intersect: false,
+      },
+      hover: {
+        mode: "nearest",
+        intersect: true,
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              fontColor: "rgba(255,255,255,.7)",
+            },
+            display: true,
+            scaleLabel: {
+              display: false,
+              labelString: "1번1",
+              fontColor: "white",
+            },
+            gridLines: {
+              display: false,
+              borderDash: [2],
+              borderDashOffset: [2],
+              color: "rgba(33, 37, 41, 0.3)",
+              zeroLineColor: "rgba(0, 0, 0, 0)",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              fontColor: "rgba(255,255,255,.7)",
+              suggestedMin: 40,    // minimum will be 30, unless there is a lower value.
+              stepSize: 10,
+              max: 100,
+            },
+            display: true,
+            scaleLabel: {
+              display: false,
+              labelString: "Value",
+              fontColor: "white",
+            },
+            gridLines: {
+              borderDash: [3],
+              borderDashOffset: [3],
+              drawBorder: false,
+              color: "rgba(255, 255, 255, 0.15)",
+              zeroLineColor: "rgba(33, 37, 41, 0)",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
+          },
+        ],
+      },
+    },
+  };
+  React.useEffect(() => {
+    call("/teachers/average-graph", "POST", {"grade" : 3, "examSubject" : "ENGLISH"}).then((response) => {
+      for (var i = 0; i < response.result.eachAverageScoreList.length; i++) {
+        config.data.labels.push(response.result.eachAverageScoreList[i].examNumber + "회차");
+        config.data.datasets[0].data.push(response.result.eachAverageScoreList[i].averageScore);
+      }
+      var ctx = document.getElementById("line-chart").getContext("2d");
+      window.myLine = new Chart(ctx, config);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
@@ -119,9 +111,9 @@ export  default function TeacherEngLineChart() {
           <div className="flex flex-wrap items-center">
             <div className="relative w-full max-w-full flex-grow flex-1">
               <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold bg-cyan-600">
-                Overview
+                성적 그래프
               </h6>
-              <h2 className="text-white text-xl font-semibold">Sales value</h2>
+              <h2 className="text-white text-xl font-semibold">회차별 평균 점수</h2>
             </div>
           </div>
         </div>
