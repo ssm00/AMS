3232fcb84882356b9ef8e9b00dece2d909c3497e
ssm00 <titan0724@mju.ac.kr>
2023-07-08 22:08:00 +0900

    Update Login.js

--- ams-frontend/src/auth/Login.js
+++ ams-frontend/src/auth/Login.js
@@ -67,7 +67,7 @@ export default function Login() {
                           <small>Or sign in with credentials</small>
                         </div>
                         {/*submit*/}
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit} method="post">
                           <div className="relative w-full mb-3">
                             <label
                               className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
