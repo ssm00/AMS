1f2c0370a4550e1334afd1b3ab867680224a6dbe
ssm00 <titan0724@mju.ac.kr>
2023-07-08 22:08:26 +0900

    Update TeacherMain.js

--- ams-frontend/src/teacher/TeacherMain.js
+++ ams-frontend/src/teacher/TeacherMain.js
@@ -7,6 +7,8 @@ import TeacherTopWrongRate from "../components/Cards/TeacherTopWrongRate.js";
 import Sidebar from "../components/Sidebar/Sidebar";
 import HeaderStats from "../components/Headers/HeaderStats";
 import TeacherTopUserName from "../components/Cards/TeacherTopUserName";
+import TeacherEngBarChart from "../components/Cards/TeacherEngBarChart";
+import TeacherNormChart from "../components/Cards/TeacherNormChart";
 
 export default function TeacherMain() {
     return (
@@ -18,12 +20,26 @@ export default function TeacherMain() {
                     <div className="flex flex-wrap mt-4">
                         <div className="w-full xl:w-4/12 px-4">
                             <TeacherTopUserName
-                                examNumber={1}
+                                examNumber={2}
                             />
                         </div>
                         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
                             <TeacherTopWrongRate
-                                examNumber={1}
+                                examNumber={2}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap">
+                        <div className="w-full mb-12 xl:mb-0 px-4">
+                            <TeacherEngBarChart
+                                examNumber={2}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap">
+                        <div className="w-full mb-12 xl:mb-0 px-4">
+                            <TeacherNormChart
+                                examNumber={2}
                             />
                         </div>
                     </div>
