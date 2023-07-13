248a265eb78e0ced2bb7f53aa926916eb31a162a
ssm00 <titan0724@mju.ac.kr>
2023-07-14 00:28:58 +0900

    Update AppRouter.js

--- ams-frontend/src/AppRouter.js
+++ ams-frontend/src/AppRouter.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
-import TeacherMain from "./teacher/TeacherMain";
+import TeacherEngMain from "./teacher/TeacherEngMain";
 import Login from "./auth/Login";
 import SignUp from "./auth/SignUp";
-import TeacherEngAnswer from "./teacher/TeacherEngAnswer";
+import TeacherEngAnswerMain from "./teacher/views/TeacherEngAnswerMain";
 import StudentEngAnswer from "./teacher/StudentEngAnswer";
 
 import EnglishMain from "./student/views/english";
@@ -17,15 +17,21 @@ class AppRouter extends React.Component {
                     <div>
                         {/* React-router-dom V6 이후 Switch -> routes로 바뀜  Route 안에 element 사용 */}
                         <Routes>
-                            <Route path="/teacher" element={<TeacherMain />} />
+                            {/*all*/}
                             <Route path="/users/*" element={<Login />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/sign-up" element={<SignUp />} />
-                            <Route path="/teacher/answer" element={<TeacherEngAnswer />} />
-                            <Route path="/student/answer" element={<StudentEngAnswer />} />
-                            <Route path="/student/english/main" element={<EnglishMain />} />
+                            <Route path="/" element={<Navigate replace to="/login"/>} />
+
+                            {/*teacher*/}
+                            <Route path="/teacher/english/main" element={<TeacherEngMain />} />
+                            <Route path="/teacher/english/answer" element={<TeacherEngAnswerMain />} />
+
+                            {/*student*/}
                             <Route path="/student" element={<Navigate replace to="/student/english/main"/>} />
-                            <Route path="/" element={<Navigate replace to="/login"/>} />   
+                            <Route path="/student/english/answer" element={<StudentEngAnswer />} />
+                            <Route path="/student/english/main" element={<EnglishMain />} />
+
                         </Routes>
                     </div>
                 </Router>
