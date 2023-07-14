900c4427fd28e5f5f68243a440b6090649a6b375
ssm00 <titan0724@mju.ac.kr>
2023-07-14 21:55:27 +0900

    Create TeacherEngModifyMain.js

---
+++ ams-frontend/src/teacher/views/TeacherEngModifyMain.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+// components
+
+import TeacherEngSidebar from "../component/Sidebar/TeacherEngSidebar";
+import TeacherHeaderStats from "../component/Header/TeacherHeaderStats";
+import TeacherModifyForm from "../component/AnswerInput/TeacherModifyForm";
+
+export default function TeacherEngModifyMain() {
+    const [examNumber, setExamNumber] = React.useState(1);
+    function changeExamNumber(examNumber){
+        setExamNumber(examNumber);
+    }
+    return (
+        <>
+            <TeacherEngSidebar />
+            <div className="relative md:ml-64 bg-blueGray-100">
+                <TeacherHeaderStats
+                    changeExamNumber={changeExamNumber}
+                />
+                <div className="px-4 md:px-10 mx-auto w-full -m-24">
+                    <div className="flex flex-wrap">
+                        <div className="w-full mb-12 xl:mb-0 px-4">
+                            <TeacherModifyForm
+                                examNumber={examNumber}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
